d3447c0d3eb912c60b877f5645961d204841f427
Maximiliano Molina <hpmaxi@gmail.com>
2020-02-14 18:26:06 -0300

    Updates

---
+++ precache-manifest.d67cdd5f8d010d532db46e50cb9f2495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f559559d82667ee7c880b7949e3cc175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51796daa1033e657488e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "22d699edb71bbad9284a",
+    "url": "/static/js/2.6515640b.chunk.js"
+  },
+  {
+    "revision": "c205a8831765fad77c4bb4c306f8092c",
+    "url": "/static/js/2.6515640b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51796daa1033e657488e",
+    "url": "/static/js/main.afe13be3.chunk.js"
+  },
+  {
+    "revision": "14061363735c140d8009",
+    "url": "/static/js/runtime-main.5383be82.js"
+  }
+]);
