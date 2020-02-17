1d9429628c46f61c1fcbb659aff3a4594fa121b9
Maximiliano Molina <hpmaxi@gmail.com>
2020-02-17 16:05:41 -0300

    Change dai contract and abi

--- src/networks.js
+++ src/networks.js
@@ -5,8 +5,8 @@ export const networks = {
       abi: ["function allocateTo(address to, uint amount)"]
     },
     "42": {
-      address: "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99",
-      abi: ["function allocateTo(address to, uint amount)"]
+      address: "0x29200B8486Ec4f3A13E11f03FB0017C15a99C435",
+      abi: ["function mint(address to, uint amount)"]
     }
   },
   BAT: {
